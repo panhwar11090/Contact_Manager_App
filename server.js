aeee03bb5b455fb4b111bf4d666625310f47ce1c
panhwar11090 <11090@kiet.edu.pk>
2023-10-24 04:40:29 +0500

    production

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 
@@ -14,7 +14,6 @@ app.use(express.json({extended: false}));
 // Connect to mongoDB
 connectDB();
 
-app.get('/', (req,res)=> res.json({msg: 'Welcome to contact manager API'}));
 
 
 
@@ -23,6 +22,13 @@ app.get('/', (req,res)=> res.json({msg: 'Welcome to contact manager API'}));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+//Serve static files on production server
+if(process.env.NODE_ENV === 'production') {
+    // Set Static Folder
+    app.use(express.static('client/build'));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 
 const PORT = process.env.PORT || 5000;
